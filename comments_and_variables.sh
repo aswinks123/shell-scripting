1daf50437dc8498584bb06e30813584aafa106e1
Aswinks1995 <aswinks1995@outlook.com>
2023-01-15 21:30:10 -0500

    comments and variables

---
+++ comments_and_variables.sh
@@ -0,0 +1,19 @@
+#Comments and variables
+
+#!bin/bash
+
+#printing a text
+echo "Hello world!"
+
+#Variables are 2 types system(capital case) and user defined variables
+
+echo $BASH #system variable
+echo $HOME
+
+#Variables that accept value and print it
+name=Aswin  #user defined variable
+echo  My name is $name
+
+
+
+
